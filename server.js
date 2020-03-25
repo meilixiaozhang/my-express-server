7d13d908c4414b624b186e1309b14a679666291a
meilixiaozhang <redpangci@gmail.com>
2020-03-25 17:57:03 +1100

    Update server.js

--- server.js
+++ server.js
@@ -2,4 +2,23 @@
 
 const express = require('express');
 const app = express();
-app.listen(3000);
+// "/" refers to the home route
+app.get("/", function(request,response){
+  response.send("<h1>Hello, World</h1>")
+});
+
+app.get("/contact", function(request,response){
+  response.send("Contact me at: xiaoranzhang08@gmail.com")
+})
+
+app.get("/about", function(req, res){
+  res.send("I am beautiful Xiao Zhang")
+})
+
+app.get("/hobbies", function(req, res){
+  res.send("baking eating sleeping all day")
+})
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
